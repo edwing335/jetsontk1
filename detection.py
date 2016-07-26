8ece501da1466ad0b7a864a8ed8416a41f4d0dfc
david <lexuszhi1990@gmail.com>
2016-07-26 10:23:16 +0800

    update detection

--- detection.py
+++ detection.py
@@ -1,12 +1,9 @@
 import numpy as np
 
 def check_object_status(tracking_data_list):
-  debug = False
-  if debug:
-    print("start to detect object %d"%(len(tracking_data_list)))
-
+  debug = True
   if len(tracking_data_list) > 5:
-    angle_list = [abs(tracking_data_list[i].get('angle') - 45) for i in xrange(5)]
+    angle_list = [abs(tracking_data_list[i].get('angle')-45) for i in xrange(5)]
     angle_list.remove(max(angle_list))
     angle_list.remove(min(angle_list))
     mean_angle = np.mean(angle_list)
@@ -17,10 +14,10 @@ def check_object_status(tracking_data_list):
     height_width_ratio = np.mean(height_width_ratio_list)
 
     if debug:
-      print("height_width_ratio is %f"%(height_width_ratio))
+      print("height_width_ratio is %f\n"%(height_width_ratio))
       print("mean_angle is %f"%(mean_angle))
 
-    if np.mean(angle_list) < 15 and height_width_ratio>1.25:
+    if np.mean(angle_list) < 15 and height_width_ratio < 0.7:
       print('the robot is falling down')
       return False
 
